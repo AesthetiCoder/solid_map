cf895442e478b658e21f6177772e09f77a52b009
Luis Monsalve <luismonsalve1941@gmail.com>
2023-03-21 22:18:16 -0400

    :tada: New proyects

---
+++ src/map.tsx
@@ -0,0 +1,13 @@
+const Map = () => (
+  <div class='footer-map'>
+    <iframe
+      src='https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d59300.46446226757!2d-90.1990071630986!3d38.6351569191218!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sve!4v1679451144286!5m2!1sen!2sve'
+      class='footer-map__map'
+      loading='lazy'
+      frameborder='0'
+      referrerpolicy='no-referrer-when-downgrade'
+    />
+  </div>
+);
+
+export { Map };
